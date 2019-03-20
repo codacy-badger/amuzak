c046d84aebceb77ff96196e1a2a7030cdf150a3c
lachlan <lachlan.00@gmail.com>
2019-03-21 09:28:46 +1000

    https for jquery migration

--- templates/install_footer.inc.php
+++ templates/install_footer.inc.php
@@ -5,8 +5,8 @@
     <script src="<?php echo $web_path; ?>/lib/components/jquery/jquery.min.js"></script>
     <script src="<?php echo $web_path; ?>/lib/components/bootstrap/js/bootstrap.min.js"></script>
     <script src="<?php echo $web_path; ?>/lib/javascript/base.js" type="text/javascript"></script>
-    <script src="http://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="http://code.jquery.com/jquery-migrate-1.4.1.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/jquery-migrate-1.4.1.js"></script>
     <?php
         if (isset($jsEnd) && !empty($jsEnd) && is_array($jsEnd)) {
             foreach ($jsEnd as $js) {
