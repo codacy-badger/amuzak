390ca6ea685448be9343e3209fb88bab4e6af7a0
lachlan-00 <lachlan.00@gmail.com>
2019-03-22 22:07:20 +1000

    scale for mobile to fit all option divs/tables

--- templates/header.inc.php
+++ templates/header.inc.php
@@ -46,7 +46,7 @@ $_SESSION['login'] = false;
             }
         ?>
         <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=<?php echo AmpConfig::get('site_charset'); ?>" />
-        <meta name="viewport" content="width=850, initial-scale=1.0">
+        <meta name="viewport" content="width=930, initial-scale=1.0">
         <title><?php echo AmpConfig::get('site_title'); ?> - <?php echo $location['title']; ?></title>
 
         <?php require_once AmpConfig::get('prefix') . UI::find_template('stylesheets.inc.php'); ?>
