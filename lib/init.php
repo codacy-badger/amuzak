04fe544531b35c7ac3d38a72b4d0b2076a5acfd8
lachlan <lachlan.00@gmail.com>
2019-03-27 11:46:57 +1000

    4.0.2 Don't be Sad

--- lib/init.php
+++ lib/init.php
@@ -67,7 +67,7 @@ if (!empty($link)) {
 
 $results['load_time_begin'] = $load_time_begin;
 /** This is the version.... fluff nothing more... **/
-$results['version']            = '4.0.1';
+$results['version']            = '4.0.2';
 $results['int_config_version'] = '41';
 
 if (!empty($results['force_ssl'])) {
