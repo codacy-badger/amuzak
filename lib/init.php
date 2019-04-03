e9c381565db13c697297c9d52f0b068a8ee524bd
www-data <www-data@lachlandewaard.org>
2019-04-04 09:56:48 +1000

    begin 4.0.6-develoop

--- lib/init.php
+++ lib/init.php
@@ -67,7 +67,7 @@ if (!empty($link)) {
 
 $results['load_time_begin'] = $load_time_begin;
 /** This is the version.... fluff nothing more... **/
-$results['version']            = '4.0.5';
+$results['version']            = '4.0.6-develop';
 $results['int_config_version'] = '41';
 
 if (!empty($results['force_ssl'])) {
