c3f4ffc6796b7a97cf20d42f778a1281b7a6b25e
lachlan-00 <lachlan.00@gmail.com>
2019-04-03 14:09:57 +1000

    4.0.5 Neo Tokyo

--- lib/init.php
+++ lib/init.php
@@ -67,7 +67,7 @@ if (!empty($link)) {
 
 $results['load_time_begin'] = $load_time_begin;
 /** This is the version.... fluff nothing more... **/
-$results['version']            = '4.0.5-develop';
+$results['version']            = '4.0.5';
 $results['int_config_version'] = '41';
 
 if (!empty($results['force_ssl'])) {
